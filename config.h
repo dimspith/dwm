7d6026bb7cb4d2d68d0f815efa115e531fbcc6bb
dimspith <51262483+dimspith@users.noreply.github.com>
2021-01-05 14:56:39 +0200

    Change font and startup programs

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka Term Extended:size=10", "monospace:size=10" };
-static const char dmenufont[]       = "Iosevka Term Extended:size=10";
+static const char *fonts[]          = { "Hurmit NF:size=10", "monospace:size=10" };
+static const char dmenufont[]       = "Hurmit NF:size=10";
 static const char col_fg[]               = "#ebdbb2";
 static const char col_fg_sel[]           = "#fbf1c7";
 static const char col_bg[]               = "#141413";
@@ -25,7 +25,7 @@ static const char *colors[][3]           = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
