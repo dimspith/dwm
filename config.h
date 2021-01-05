81720ae0baf489318f39032e56e7b955b4b0445c
dimspith <51262483+dimspith@users.noreply.github.com>
2021-01-05 15:27:56 +0200

    Disable bottom border from bartabgroups

--- config.h
+++ config.h
@@ -54,7 +54,7 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 /* Bartabgroups properties */
 #define BARTAB_BORDERS 1       // 0 = off, 1 = on
-#define BARTAB_BOTTOMBORDER 1  // 0 = off, 1 = on
+#define BARTAB_BOTTOMBORDER 0  // 0 = off, 1 = on
 #define BARTAB_TAGSINDICATOR 1 // 0 = off, 1 = on if >1 client/view tag, 2 = always on
 #define BARTAB_TAGSPX 5        // # pixels for tag grid boxes
 #define BARTAB_TAGSROWS 3      // # rows in tag grid (9 tags, e.g. 3x3)
