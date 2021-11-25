bd167737e37e483af12fcf4dae2a48d201901217
dimspith <51262483+dimspith@users.noreply.github.com>
2021-11-25 15:06:51 +0200

    Changes

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-static const char *fonts[]          = { "SpaceMono NF:size=10:style=bold", "monospace:size=10" };
+static const char *fonts[]          = { "Iosevka Extended:size=11:style=bold", "monospace:size=10" };
 static const char dmenufont[]       = "SpaceMono NF:size=10:style=bold";
 
 
